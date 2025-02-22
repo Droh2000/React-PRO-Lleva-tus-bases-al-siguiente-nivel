9ff83d30c251e3a7f1b7802d6cb0d349c9db1102
diego ortega <diego-chido2011@hotmail.com>
2025-02-22 15:24:06 -0600

    Optimizacion y Tipado

--- 01-ReforzamientoReact/src/bases/CounterBy.tsx
+++ 01-ReforzamientoReact/src/bases/CounterBy.tsx
@@ -4,8 +4,10 @@ interface Props {
     initialValue?: number
 }
 
-interface Values {
-    counters: number,
+// Como recomendacion hay que especificar el tipado del STATE para que sepan los demas usuarios como luce
+interface CounterState {
+    // Aparte del tipo de dato tambien el nombre tiene que coincidar a la hora de implementar el tipado
+    counter: number,
     clicks: number
 }
 
@@ -13,12 +15,17 @@ interface Values {
 export const CounterBy = ({initialValue = 5}:Props) => {
 
     // En el estado requerimos mantener el Counter y la cantidad de clicks
-    const [counterState, setCounterState] = useState({
+    // Si ponemos el cursor encima del USESTATE veremos los <> que significa que es un generico con esto le podemos
+    // definir el tipo de informacion que maneje internamente
+    // Ademas tenemos la ventaja que si comentemos un error al definir las propiedades al dejar el cursor encima obtendremos un mensaje mas amigable
+    //  si nos fijamos que dice el tipo que es "(() => CounterState)" es porque con el useState podemos tener una funcion que retorne el STATE cuando lo
+    //  queramos preprocesar o asignar una funcion que retorne el estado inicial
+    const [{ counter, clicks }, setCounterState] = useState<CounterState>({
         counter: initialValue,
         clicks: 0,//Cada vez que se incremente el counter debemos de contar los clicks
     });
 
-    const handlerClick = (value:number) => {
+    const handlerClick = ( value:number ) => {
         // Tenemos que definir a fuerzas las propiedades del state que definimos
         /*
             Una forma de declarar la sintaxis seria:
@@ -41,13 +48,19 @@ export const CounterBy = ({initialValue = 5}:Props) => {
                     clicks: clicks + 1,
                 });
         */
-        setCounterState( prev => ({
-            counter: prev.counter + value,
-            clicks: prev.clicks + 1
-        }))
+       // En el argumento que antes era "prev" lo desestructuramos para sacar directamente las variables
+       // que no se nos olvide que para la desestructuracion tenemos que usar ( ) y dentro van los {}
+        setCounterState( ({ counter, clicks }) => ({
+            counter: counter + value,
+            clicks: clicks + 1
+        }));
+        // Porque no chocan el counter de aqui con el counter del useState
+        // Como estamos implementando este "counter" de arriba por el Scope  donde JS primero resuelve este que esta mas interno
+        // y despues resuelve el "counter" de afuera que sacamos del useState
     }
 
-    const { counter, clicks } = counterState;
+    // Para no hacer este paso extra se desestructuro desde la definicion del useState()
+    //const { counter, clicks } = counterState;
 
     return (
         <>
