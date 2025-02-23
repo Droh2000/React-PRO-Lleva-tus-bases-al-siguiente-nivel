b8dd60973cb6988ac99bd806ed1f5dee0a323454
diego ortega <diego-chido2011@hotmail.com>
2025-02-23 11:17:55 -0600

    Funcion Pura del Reducer

--- 01-ReforzamientoReact/src/bases/CounterReducer.tsx
+++ 01-ReforzamientoReact/src/bases/CounterReducer.tsx
@@ -16,6 +16,44 @@ const INITIAL_STATE: CounterState = {
     changes: 0,
 }
 
+// En la funcion pura del "reducer" significa que recibe un estado inicial o un estado anterior, una accion y produce otro estado
+// Recibimos como argumento el state del mismo tipo que definimos y la accion
+/* Las acciones seria por ejemplo:
+{
+    type: 'Nombre de la Accion',
+    payload: Es el dato que requiramos para manipular y puede ser opcional
+}
+Asi que nos creamos el tipo de dato para decirle a TS como se miran las acciones y a la vez controlar las acciones permitidas en el reducer
+
+En este caso las acciones las definimos en el Type y el payload lo ponemos entre llavez porque si y 
+especificarle el valor , todas las acciones las defenimos entre {} y las separamos con el operado OR |
+
+Solo estas dos tipos de acciones las vamos a permitir
+*/
+type CounterAction = 
+                   | { type: 'increaseBy', payload: { value: number; } }
+                   | { type: 'reset' } // Este es para resetear a un estado inicial
+
+const counterReducer = ( state:CounterState, action: CounterAction): CounterState => {
+    switch (action.type) {
+        // Aqui definimos lo que va a realizar cada accion y tenemos que regresar algo del tipo CounterState
+        case 'reset':    
+            return{
+                counter: 0,
+                changes: 0,
+                previous: 0
+            }
+        case 'increaseBy':    
+            return{
+                counter: 0,
+                changes: 0,
+                previous: 0
+            }
+        default:
+            return state;
+    }
+}
+
 export const CounterReducerComponent = () => {
 
     /*
@@ -30,11 +68,18 @@ export const CounterReducerComponent = () => {
                        interaccion con el mundo externo y tampoco debe de ser asyncrona
             * initialState: Este es el satado inicial que le queremos dar al useReducer
             * init: Es para implemetar el Lazyload es decir mediante una carga perezosa cuando el componente ya esta construido se llama este init 
+        
+        El "state" contiene cada una de las propiedades que definimos de counter, prevoius, changes
+            Asi que de este podemos desestructurar estas propiedades que las que se usaran 
+        la funcion "dispatch" se encrga de ejecutar las acciones
     */
-    const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+    const [{ counter }, dispatch] = useReducer(counterReducer, INITIAL_STATE);
 
     const handlerClick = () => {
-        setCounter( prev => prev + 1 );
+        // Aqui se manda la accion al reducer, esa accion es la que se envia como parametro a la funcion Reducer que se va
+        // al Switch/case y dentro ejecuta la logica correspondiente del nuevo estado y React con el useReducer sabe que el
+        // esta cambio por consecuencia tiene que redibujar lo que tenga que redibujar
+        dispatch({ type: 'reset' });
     }
 
     return (
@@ -42,7 +87,7 @@ export const CounterReducerComponent = () => {
             <h1>Counter Reducer: { counter }</h1>
 
             <button onClick={ handlerClick }>
-                +1
+                Reset
             </button>
         </>
     )
