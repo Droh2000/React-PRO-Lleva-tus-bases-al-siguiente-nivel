a6d9e734100dc428ba2dda25bc1fac862eeb637d
diego ortega <diego-chido2011@hotmail.com>
2025-02-23 10:17:46 -0600

    Parametrizar y pulir el custom hook

--- 01-ReforzamientoReact/src/bases/CounterHook.tsx
+++ 01-ReforzamientoReact/src/bases/CounterHook.tsx
@@ -2,12 +2,16 @@ import { useCounter } from "../hooks/useCounter"
 
 export const CounterHook = () => {
     // Creamos el customHook para separar la logica
-    const { counter, counterElement, handlerClick } = useCounter();
+    // Podriamos usar este "counterElement" para animar cualquier cosa, solo pasandolo en el atributo "ref=" del elemento HTML
+    // y asegurandonos de que el tipo de dato que definamos en el useRef del "useCunter" sea el correcto para el elemento HTML
+    const { counter, elementToAnimate, handlerClick } = useCounter({
+        maxCount: 15
+    });
 
     return (
         <>
             <h1>CounterHook:</h1>
-            <h2 ref={ counterElement }>{ counter }</h2>
+            <h2 ref={ elementToAnimate }>{ counter }</h2>
 
             <button onClick={ handlerClick }>
                 +1
--- 01-ReforzamientoReact/src/hooks/useCounter.ts
+++ 01-ReforzamientoReact/src/hooks/useCounter.ts
@@ -1,39 +1,68 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useLayoutEffect, useRef, useState } from "react"
 import { gsap } from 'gsap';
 
-const MAXIMUN_COUNT = 10;
+//const MAXIMUN_COUNT = 10;
 
 // Este CustomHook se creo para separar la logica que se tenia originalmente en el Counterhook
-export const useCounter = () => {
+// Como argumento recibira el valor maximo por el que no se pasara 
+export const useCounter = ({ maxCount = 10 }) => {
     const [counter, setCounter] = useState(5);
+    // Se le puso ANY para que podamos agreagr la animacion en cualquier elemento HTML
+    const elementToAnimate = useRef<any>(null);
 
-    const counterElement = useRef<HTMLHeadingElement>(null);
+    // Para que no se cree un nuevo timeline cada vez que el useEffect se dispara, asi que nos creamos otro useRef
+    // para crearlo una sola vez y almacenar la referencia en memoria
+    const tl = useRef( gsap.timeline() );
 
     const handlerClick = () => {
-        setCounter( prev => Math.min( prev + 1, MAXIMUN_COUNT ) );
+        setCounter( prev => Math.min( prev + 1, maxCount ) );
     }
 
+    // Desdes de la implementacion del useRef en el timeline nos dimos cuenta que salta varias veces esto es porque
+    // cada vez que precionamos el boton se van agregando animaciones al timeline y no deberia de dispararse cuanto el counter cambia
+    // porque crea mas animaciones de las que deberia y la animacion que creamos deberia de dispararse solo cuando algo suceda
+    // Vamos a usar este hook para asegurarnos que se ejecute cuando ya tenemos creado los elementos HTML y dentro movemos las animaciones
+    // este funciona muy parecido al useEffect y vamos a dejar la dependencia vacia para que apenas se construya el componente solo se creen una vez
+    // las animaciones
+    useLayoutEffect(()=>{
+        // Podria pasar que en el momoento que esto se crea el "counterElement" podria ser que tenga una referencia nula y esto nos podra dar un error
+        // Se puede agregar la validacion para que se ejecute solo si tenemos la referencia al elemento
+        if( !elementToAnimate.current ) return;
+
+        // Otra cosa es que apenas se ejecuta el timeline, se va a disparar asi que le ponemos el Pause al final 
+        // esto es para que al crear el componente no se ejecute sino que solo lo haga cuando cambie el counter
+        tl.current.to(elementToAnimate.current, { y: -10, duration: 0.2, ease: 'ease.out'})
+                  .to(elementToAnimate.current, { y: 0, duration: 1, ease: 'bounce.out'})
+                  .pause();
+    }, []);
+
+    // Como buena practica se recomienda que cada hook tenga una rsponsabilidad unica
     useEffect(() => {
-        if ( counter < 10 ) return;
+        //if ( counter < 10 ) return;
 
         // Por la logica que teniamos anteriormente es que cada vez que el useEffect pasa de la condicion de arriba
         // se crea una nueva instancia del timeline(), lo mejor es no crear esto dentro del useEffect sino que 
         // lo elevamos a tal punto en el que podamos mantener la referencia a su valor actual y no crear una nueva instancia
         // cada vez que este efecto se dispara (Esto tambien nos ayudara si queremos que la animacion se repita cada vez que cambia)
         
-        console.log('%cSe llego al valor maximo','color:red; background-color: black;');
+        //console.log('%cSe llego al valor maximo','color:red; background-color: black;');
 
-        const tl = gsap.timeline();
+        //const tl = gsap.timeline();
 
-        tl.to(counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out'})
-            .to(counterElement.current, { y: 0, duration: 1, ease: 'bounce.out'});
+        // Como ahora tenemos el timeline en el useRef para usalo tenemos que llamar el .current
+        //tl.current.to(counterElement.current, { y: -10, duration: 0.2, ease: 'ease.out'})
+        //    .to(counterElement.current, { y: 0, duration: 1, ease: 'bounce.out'});
+
+        // Para ejecutar la animacion cada vez que el counter cambia tenemos la funcion PLAY() pero hay que pasarle como argumentos
+        // que inicie desde la pocicion 0 oara que cada vez que cambie el conunte se reprodusca desde el inicio
+        tl.current.play(0);
 
     }, [counter]);
 
     // Retornamos lo que va a usar el CounterHook
     return {
         counter,
-        counterElement,
+        elementToAnimate,
         handlerClick,
     }
 }
