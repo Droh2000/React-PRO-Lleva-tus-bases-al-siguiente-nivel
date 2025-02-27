fec7566343fe36ef3e5996928ca75b96679be344
gomezfarm <gomez.farm@outlook.com>
2025-02-27 09:20:25 -0600

    Seccion 4: Lazyload - Chunks

--- 02-react-app/src/routes/Navigation.tsx
+++ 02-react-app/src/routes/Navigation.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, NavLink, Routes, Navigate } from 'react-router-dom';
 
-import logo from '../assets/react.svg'
+import logo from '../assets/react.svg';
+
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
 
 export const Navigation = () => {
     return (
@@ -17,27 +19,41 @@ export const Navigation = () => {
 
                         La logica implementada dentro del className es que de manera condicional mostramos una clase
                         que creamos en el CSS llamada .nav-active, con el argumento "isActiva" regresamos o no la clase
+
+                        En la carpeta 01-lazyload es un modulo en donde vamos a agrupar todo lo rleacionado a Lazyload
+                        Dentro de pages creamos varios componentes que son los que configuramos como rutas aqui
                     */}
                     <ul>
                         <li>
-                            <NavLink to='/home' className={ ({ isActive }) => !isActive ? 'nav-active' : ''} >Home</NavLink>
+                            <NavLink to='/lazy1' className={ ({ isActive }) => !isActive ? 'nav-active' : ''} >Lazy 1</NavLink>
                         </li>
                         <li>
-                            <NavLink to='/about' className={ ({ isActive }) => !isActive ? 'nav-active' : ''}>About</NavLink>
+                            <NavLink to='/lazy2' className={ ({ isActive }) => !isActive ? 'nav-active' : ''}>Lazy 2</NavLink>
                         </li>
                         <li>
-                            <NavLink to='/users' className={ ({ isActive }) => !isActive ? 'nav-active' : ''}>Users</NavLink>
+                            <NavLink to='/lazy3' className={ ({ isActive }) => !isActive ? 'nav-active' : ''}>Lazy 3</NavLink>
                         </li>
                     </ul>
                 </nav>
 
-                {/* Definicion de las rutas, aqui como prueba solo nos creamos unos componentes dentro de la propiedad element */}
+                {/* Definicion de las rutas, aqui como prueba solo nos creamos unos componentes dentro de la propiedad element 
+                
+                    Sin lazyload los componentes estan siendo cargados dentro del main.chunk.js, las rutas son parte del archivo "bundle.js" que es el que sabe crear 
+                    esos componente pero con la primera carga cuando se hace el llamado a la URL nos trae toda la aplicacion y todos los componentes 
+                    Al agregar el LAzyload en cada uno de los componentes hacen que el archivo "bundle.js" se mas chico y por ende carge mas rapido la aplicacion
+                    Pero hay que saber que si segmentamos mas nuestra la aplicacion entonces hay mas tiempo de carga que va a estar separado en diferentes partes del tiempo
+
+                    El chiste esta que en modulos donde todo usuario va a tener acceso hay que dejarlo que se carge desde el inicio pero ya en partes que son de acceso restringido
+                    o donde puede que el usuario nunca entre, es donde podemos implementar el LAzyload pero por modulo completo no por componente, asi tenemos todo lista ya cargado
+                    en una unica carga y no tener cargado de manera segmentada
+                */}
                 <Routes>
-                    <Route path='about' element={ <h1>About Page</h1> } />
-                    <Route path='users' element={ <h1>Users Page</h1> } />
-                    <Route path='home' element={ <h1>Home Page</h1> } />
+                    {/* Cada ruta la mandamos a su componente correspondiente */}
+                    <Route path='lazy1' element={ <LazyPage1 /> } />
+                    <Route path='lazy2' element={ <LazyPage2 /> } />
+                    <Route path='lazy3' element={ <LazyPage3 /> } />
                     {/* Esto es para cualquier otra ruta que no sea reconocida, lo mandamos al Home y con el replace es para que no pueda regresar */}
-                    <Route path='/*' element={ <Navigate to='/home' replace /> } />
+                    <Route path='/*' element={ <Navigate to='/lazy1' replace /> } />
                 </Routes>
             </div>
         </BrowserRouter>
