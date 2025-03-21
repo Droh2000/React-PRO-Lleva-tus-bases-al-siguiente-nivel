6d976917bba59abb1676a6bb91e28398cc9b2d03
gomezfarm <gomez.farm@outlook.com>
2025-03-21 12:50:51 -0600

    Formik - Validaciones Manuales

--- 08-formik/src/03-forms/pages/FormikBasicPage.tsx
+++ 08-formik/src/03-forms/pages/FormikBasicPage.tsx
@@ -1,9 +1,43 @@
 import '../styles/styles.css';
 // Importamos su customHook
-import { useFormik } from 'formik';
+import { FormikErrors, useFormik } from 'formik';
 
+interface FormValues {
+    firstName: string,
+    lastName: string,
+    email: string,
+}
 
 export const FormikBasicPage = () => {
+
+    // Esta funcion recibe los values del formulario, el tipo de dato del argumento serian los campos que definimos
+    // Esto nos ahorra tener que hacer una validacion en cada input y luego hacer las validaciones en el Submit ya que todo se hace aqui
+    const validate = ( values: FormValues ) => {
+
+        // Esta funcion tiene que regresar los errores, este objeto debe de tener algun formato porque lo tiene que interpretar Formik
+        const errors: FormikErrors<FormValues> = {};
+
+        // Si el campo no existe
+        if( !values.firstName ){
+            errors.firstName = 'Required';
+        }else if( values.firstName.length >= 15 ){
+            errors.firstName = 'Must be 15 characters or less';
+        }
+
+        if( !values.lastName ){
+            errors.lastName = 'Required';
+        }else if( values.lastName.length >= 10 ){
+            errors.lastName = 'Must be 10 characters or less';
+        }
+
+        if(!values.email) {
+            errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Invalid email address';
+        }
+
+        return errors;
+    }
 
     // Le tenemos que mandar al Hook cierta configuracion 
     const formik = useFormik({
@@ -17,7 +51,9 @@ export const FormikBasicPage = () => {
         // se va a ejecutar cuando el formulario pase todas las reglas de validacion
         onSubmit: (values) => {
 
-        }
+        },
+        // En esta funcion creamos las validaciones.
+        validate
     });
 
     return (
