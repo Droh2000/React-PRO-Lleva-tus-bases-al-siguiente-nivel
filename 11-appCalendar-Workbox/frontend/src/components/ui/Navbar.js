de33e5665bc77ea8fa8ed85a9141bd6f5be2648a
gomezfarm <gomez.farm@outlook.com>
2025-03-30 16:31:54 -0600

    Indicar en la interface si es Online u Offline

--- 11-appCalendar-Workbox/frontend/src/components/ui/Navbar.js
+++ 11-appCalendar-Workbox/frontend/src/components/ui/Navbar.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { startLogout } from '../../actions/auth';
 
+// Hire Order Component para mostrar cuando esta online y offline
+import { Offline, Online } from 'react-detect-offline';
+
 export const Navbar = () => {
 
     const dispatch = useDispatch();
@@ -16,6 +19,20 @@ export const Navbar = () => {
             <span className="navbar-brand">
                 { name }
             </span>
+
+            {/*
+                Dentro de cada etiqueta le podemos meter cualquier tipo de contenido que se mostrara ya automaticamente si esta 
+                online u offline
+                Con esto podremos modificar el Redux, agreagr mensajes para que en cada peticion decirle al usuario que se gaurdar en cache
+                entre otras cosas
+            */}
+            <Online>
+                <span className='text-success'>Online</span>
+            </Online>
+
+            <Offline>
+            <span className='text-danger'>Offline</span>
+            </Offline>
             
             <button 
                 className="btn btn-outline-danger"
