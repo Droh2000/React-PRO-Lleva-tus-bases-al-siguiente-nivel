4db4bc79cecb1efe264eb6b02348e7f8a4b634ef
gomezfarm <gomez.farm@outlook.com>
2025-03-31 16:10:34 -0600

    Ubicarse y Activar el Lugar Seleccionado

--- 12-appMapas/src/components/SearchResult.tsx
+++ 12-appMapas/src/components/SearchResult.tsx
@@ -1,10 +1,28 @@
-import { useContext } from "react"
-import { PlacesContext } from "../context";
+import { useContext, useState } from "react"
+import { MapContext, PlacesContext } from "../context";
+import { Feature } from "../interfaces/places";
 
 export const SearchResult = () => {
 
     // Requerimos el contexto para sacar los lugares
     const { places, isLoadingPlaces } = useContext( PlacesContext );
+
+    // Queremos que cuando se hace click en el cuadro (Que sale en los resultados de busquedad), lleve al usuario a ese lugar en especifico donde esta el marcador
+    // asi que vamos a ocupar el mapa y por tanto el contexto correspondiente
+    const { map } = useContext( MapContext );
+
+    // Esto es para que cuando el usuario haga click en el cuadro este se seleccione de otro color para distinguirlo de los demas
+    const [ activeId, setActiveId ] = useState('');
+
+    const onPlacedClicked = ( place: Feature ) => {
+        // La propiedad Center de abajo esta esperando que le pasemos la latotiud y longitud
+        const [ lng, lat ] = place.center;
+        setActiveId( place.id );// Obtenemos el lugar al que se le hizo click
+        map?.flyTo({
+            zoom: 14,
+            center: [lng, lat]
+        });
+    } 
 
     // Hay detectar si es True para poner que se esta cargando
     if( isLoadingPlaces ){
@@ -30,19 +48,21 @@ export const SearchResult = () => {
                 places.map( place => (
                     <li
                         key={ place.id }
-                        className="list-group-item list-group-item-action"
+                        // Agregamos condicion para seleccionar el lugar al que le dimos click (Cuando sea true nos agregara la clase 'active')
+                        className={`list-group-item list-group-item-action pointer ${ (activeId === place.id) ? 'active' : '' }`}
+                        onClick={ () => onPlacedClicked(place) }
                     >
                         <h6>{ place.text_es }</h6>
                         <p
-                            className="text-muted"
                             style={{
                                 fontSize: '12px'
                             }}
                         >
                             { place.place_name }
                         </p>
-
-                        <button className="btn btn-outline-primary btn-sm">
+                        
+                        {/* Cambiamos el color del boton segun si esta seleccionada la caja o no */}
+                        <button className={`btn btn-sm ${ activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary'}`}>
                             Direcciones
                         </button>
                     </li>
