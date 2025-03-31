ea23bc9537ebd39dff28b730cae05b929aaaf447
gomezfarm <gomez.farm@outlook.com>
2025-03-31 08:01:27 -0600

    Seccion 13: Aplicacion de Mapas

---
+++ 12-appMapas/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='App'>
+        <header className='App-Header'>
+          <h1>Hola Mundo</h1>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
