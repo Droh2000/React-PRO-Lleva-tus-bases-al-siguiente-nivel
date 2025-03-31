76db8a723eee3d21b2755afe33ed59d263e3d2ee
gomezfarm <gomez.farm@outlook.com>
2025-03-31 11:03:50 -0600

    Mostrar Mapa de Mapbox

--- 12-appMapas/src/components/MapView.tsx
+++ 12-appMapas/src/components/MapView.tsx
@@ -1,14 +1,35 @@
 
 // Aqui requerimos acceso la informacion que se encuentra en el context provider, en la parte del PalcesProvider
 
-import { useContext } from "react";
+import { useContext, useLayoutEffect, useRef } from "react";
 import { PlacesContext } from "../context";
 import { Loading } from "./Loading";
+import mapboxgl from "mapbox-gl";
 
 // requerimos saber si esta en Loading o si no esta mostrar los datos del userLocation
 export const MapView = () => {
 
     const { isLoading, useLocation } = useContext( PlacesContext );
+
+    // Por como esta la implementacion en la documentacion, podriamos solo con poner un ID en los DIV que estan dentro del return
+    // pero como queremos que se puedan generar X cantidad de mapas y esto al ser ID solo se renderiza una sola vez
+    // asi que ocupamos cambiar la implementacion manteniendo la referencia a ese elemento DIV para no hacerlo por el ID
+    const mapDiv = useRef<HTMLDivElement>(null); // Al inicio tendra null
+
+    // Para poder renderizar el mapa necesitamos esperarnos a que la referencia tenga un valor, que el componente haya sido montado
+    // pero como queremos esperarnos que el componente ya tengalas dimenciones finales usaremos este hook
+    useLayoutEffect(() => {
+        // Si esta en true entonces esta cargando y no hacemos nada pero si esta en False mandamos a iniciar el mapa
+        if(!isLoading){
+            // Este codigo lo sacamos de la documentacion
+            const map = new mapboxgl.Map({
+                container: mapDiv.current!, // Como sabemos que si va a tener un valor le ponemos el !
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: useLocation,
+                zoom: 14
+            });
+        }
+    }, [isLoading]);
 
     // En caso de no tener los datos, seria bueno mostrar un Loading
     if( isLoading ){
@@ -16,7 +37,15 @@ export const MapView = () => {
     }
 
     return (
-        <div>
+        <div ref={ mapDiv }
+            style={{
+                height: '100vh',
+                left: 0,
+                position: 'fixed',
+                top: 0,
+                width: '100vw'
+            }}
+        >
             { useLocation?.join(',') }
         </div>
     )
--- 12-appMapas/src/main.tsx
+++ 12-appMapas/src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MapsApp } from './MapsApp'
 
+// Aqui los colocamos porque es el punto mas alto de la aplicacion
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'TOKEN_QUE_NO_SE_PUSO';
+
 // Si el navegador que esta usando el usuario no tiene la opcion de la geolocalizacion
 // ya que de ahi es donde sacamos para pedirle permisos de la geolozalizacion
 if( !navigator.geolocation ){
